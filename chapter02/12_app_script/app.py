c6f8a720bae9c8a7c786b194a6e2337a9088947f
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-09-22 21:22:54 +0300

    function import

---
+++ chapter02/12_app_script/app.py
@@ -0,0 +1,11 @@
+#import my_calculations
+#from my_calculations import my_add_function
+# from my_calculations import my_add_function as my_add
+from my_calculations import my_add_function as my_add, num1
+num2 = 10
+#res = my_calculations.my_add_function(200, 300)
+#res = my_add_function(100, 200)
+#res = my_add(100, 200)
+res = my_add(num1, num2)
+
+print(res)
