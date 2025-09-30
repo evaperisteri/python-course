73f0a3d991273234dec64763185ba9c020445732
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-09-30 22:02:57 +0300

    immutability

---
+++ chapter03/02_list_demo.py
@@ -0,0 +1,23 @@
+def print_id(variable_name, variable):
+    print(f"id({variable_name}) = {id(variable)}")
+
+def main():
+    original_list = [1, 2]
+    new_list = original_list
+    
+    print_id("original_list", original_list)
+    print_id("new list", new_list)
+    print("------------")
+    new_list[0] = 100
+    print_id("original_list", original_list)
+    print_id("new list", new_list)
+
+    print(original_list)
+    print(new_list)
+
+    temp_list = [100, 2]
+    print_id("temp list", temp_list)
+    print_id("original list item 0", original_list[0])
+    print_id("original list item 0", temp_list[0])
+if __name__ == "__main__":
+    main()
