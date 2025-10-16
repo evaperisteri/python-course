b2d95b3ac725bab5f0a29f366c2b78c9d5cfddff
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-10-16 21:05:24 +0300

    lambdas intro

---
+++ chapter04/13_lambda_demo.py
@@ -0,0 +1,12 @@
+def my_power(base, exp):
+    return base ** exp
+
+power_to = lambda base, exp: base ** exp
+
+def main():
+    print(my_power(2, 10))
+
+    print(power_to(2, 10))
+
+if __name__ =="__main__":
+    main()
