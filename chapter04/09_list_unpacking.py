619c4e22a03ae5ee933c76149574a6264419df47
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-10-16 19:21:21 +0300

    comments on list unpacking

--- chapter04/09_list_unpacking.py
+++ chapter04/09_list_unpacking.py
@@ -20,5 +20,7 @@ print(f"a = {a}, b = {b}")
 print(f"a = {a}, b = {b}, c = {c}")
 
 first, *middle, last = my_list
-
+# * gives us a list despite the type of mutable object. either we have a list or a tupple
+# my_list = [1, 2, 3] returns first = 1 , middle = [2], last = 3
+# my_list = [1, 2] returns first = 1 , middle = [], last = 2
 print(f"1st element = {first}, middle part = {middle}, last element = {last}")
